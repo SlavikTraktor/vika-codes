60cd3b969c3e1a8479c2e2b69f30ddaf997f7591
Sviatoslav Sobakin <slavasobb@gmail.com>
2022-03-19 17:42:56 +0300

    cycles and arrays

---
+++ 4_cycles.js
@@ -0,0 +1,11 @@
+let i = 0;
+while (i < 5) {
+  console.log("резать яблоко", i + 1);
+  //   i = i + 1;
+  i += 2;
+  //   i++;
+}
+
+for (let i = 1; i < 5; i += 2) {
+  console.log("резать яблоко", i + 1);
+}
---
+++ 5_cycles_and_arrays.js
@@ -0,0 +1,8 @@
+let arr = [1, 8, 4, 2, 6, 3];
+
+let len = arr.length;
+
+for (let i = 1; i < len; i++) {
+  console.log(arr[i - 1] * 2);
+}
+
