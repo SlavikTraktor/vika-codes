cdf6b3723806320a7c355fb1a188da727fa909ad
Sviatoslav Sobakin <sviatoslav.sobakin@paxful.com>
2022-04-01 21:35:07 +0300

    template strings

---
+++ 6_template_strings.js
@@ -0,0 +1,8 @@
+let nameStr = "Vika";
+
+let greetings = "Hello, " + nameStr + "!";
+let greetings2 = `Hello, ${nameStr}! ${nameStr} ${"kek"} ${228}`;
+
+console.log(greetings);
+console.log(greetings2);
+console.log(greetings === greetings2);
