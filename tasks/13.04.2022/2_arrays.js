c76b7255b9b0e54adc49fb4921b3b4da01588354
Slava Sobakin <slavasobb@gmail.com>
2022-04-15 16:43:06 +0300

    first task solved

--- tasks/13.04.2022/2_arrays.js
+++ tasks/13.04.2022/2_arrays.js
@@ -1,10 +1,14 @@
 let arr = [1, 4, 2, 4, 8, 5, -3, 3, 1, 8, 10, 8];
-let arr_new =[];
-// К каждому числу больше 5 прибавить 8
-let x;
-for (let i = 0; i < arr.length; i++) {
- let x = arr[i] > 5
-}
 
-  
-console.log (x);
+// К каждому числу больше 5 прибавить 8
+
+for (let i = 0; i < arr.length; i++) {
+  //   let x = arr[i] > 5;
+  //   if (x == true) {
+  //     arr[i] = arr[i] + 8;
+  if (arr[i] > 5) {
+    arr[i] = arr[i] + 8;
+    // }
+  }
+}
+console.log(arr);
